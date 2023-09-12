6a67e90341c2749fa081198d90a0046d7211a910
sumaia <v-sumaia@microsoft.com>
2023-09-12 15:54:01 +0300

    adding "keyboardInterrupt" exception handling

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import functions as fun
 from variables import *
-
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.language.conversations import ConversationAnalysisClient
 
-endpoint = API_ENDPOINT
-credential = AzureKeyCredential(API_KEY)
-client = ConversationAnalysisClient(endpoint, credential)
+def main():
+    try:
+        endpoint = API_ENDPOINT
+        credential = AzureKeyCredential(API_KEY)
+        client = ConversationAnalysisClient(endpoint, credential)
+        print('Bot: say something ')
+        while True:
+            response=input('You: ')
+            print(f'Bot: you said ', response)
+    except KeyboardInterrupt:
+        print ('\nOk Bye')
+    else:
+        print ('No exceptions are caught')
 
-print(endpoint)
-print('Bot: say something ')
-while True:
-    response=input('You: ')
-    print(f'Bot: you said ', response)
+if __name__ == '__main__':
+    main()
 
