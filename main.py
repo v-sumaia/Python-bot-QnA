2844fc9cb89daa51ea3cc64c8e6fcc9ce667c6d0
sumaia <v-sumaia@microsoft.com>
2023-09-12 15:26:24 +0300

    adding azure client class

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import functions as fun
+import os
+
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.conversations import ConversationAnalysisClient
+
+endpoint = "https://<my-custom-subdomain>.cognitiveservices.azure.com/"
+credential = AzureKeyCredential("<api-key>")
+client = ConversationAnalysisClient(endpoint, credential)
 
 print('Bot: say something ')
 while True:
