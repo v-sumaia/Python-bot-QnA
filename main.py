920a5756d4af0a9e23a9e79512ca8cd07665b5f5
sumaia <v-sumaia@microsoft.com>
2023-09-15 16:54:52 +0300

    important

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import functions as fun
-import os
-import variables
-
+from variables import *
 from azure.core.credentials import AzureKeyCredential
-from azure.ai.language.conversations import ConversationAnalysisClient
+from azure.ai.language.questionanswering import QuestionAnsweringClient
 
-endpoint = variables.get('API_ENDPOINT')
-credential = AzureKeyCredential(variables.get('API_KEY'))
-client = ConversationAnalysisClient(endpoint, credential)
+def main():
+    try:   
+        endpoint = API_ENDPOINT
+        credential = AzureKeyCredential(API_KEY)
+        client = QuestionAnsweringClient(endpoint, credential)
 
-print('Bot: say something ')
-while True:
-    response=input('You: ')
-    print(f'Bot: you said ', response)
+        response = ''
+        print('Bot: say something.')
+        while True:
+            response = input('You: ')
+            if response != 'exit' and response != 'Exit':
+                output = fun.get_response_from_bot(response)
+                for candidate in output:
+                    print("Bot: {}".format(candidate["answer"]))
+            else:
+                print("Bot: Ok Bye")
+                exit(0)
 
+    except KeyboardInterrupt:
+        print('\nOk Bye')
+    except TypeError:
+        print('Enter a valid response.')
+
+
+if __name__ == '__main__':
+    main()
