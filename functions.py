1312c39396a1887aafcc02d6e2d53588d1538a6e
sumaia <v-sumaia@microsoft.com>
2023-09-15 16:46:45 +0300

    hiding important key

--- functions.py
+++ functions.py
@@ -6,9 +6,6 @@ def get_response_from_bot(question):
 
     url = BOT_PREDECTION_URL
     payload = "{ 'question': \"" + question + "\"" "}"
-    headers = {
-    'Ocp-Apim-Subscription-Key': 'ad9796e991994837aebc45e676ea95b5',
-    'Content-Type': 'application/json'
-    }
+    headers = { 'Ocp-Apim-Subscription-Key': OCP_APIM_SUBSCRIPTION_KEY , 'Content-Type': 'application/json' }
     response = requests.request("POST", url, headers=headers, data=payload)
     return response.json()["answers"]
